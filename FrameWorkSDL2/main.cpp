4844e8af318e9996c23da3b847fa632b4f240907
ECKLE Elias <56996771+Elias-ECKLE@users.noreply.github.com>
2021-10-19 17:57:42 +0200

    0.0.0
    Mise en  places des librairies SDL 2, SDL2 Image, SDL2 text

---
+++ FrameWorkSDL2/main.cpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include<iostream>
+#include <stdio.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+
+using namespace::std;
+
+int main(int argc, char* argv[]) {
+
+
+
+	cout << "hello world!" << endl;
+
+
+	return 0;
+}
